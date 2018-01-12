320ba91b36f57951da64ff7871d193747c96fa58
klocey <ken@weecology.org>
2018-01-11 21:11:55 -0500

    added image gif for DominanceReplacement model -KL

--- PythonCode/Image_analysis/python/Image_Erasure.py
+++ PythonCode/Image_analysis/python/Image_Erasure.py
@@ -1,12 +1,11 @@
-import numpy as np
+from __future__ import division
 import matplotlib.pyplot as plt
-import matplotlib.colors as colors
 import matplotlib.animation as animation
 import numpy as np
 from PIL import Image
+from random import randint
 import sys
 from os.path import expanduser
-
 
 mydir = expanduser("~/")
 GenPath = mydir + "GitHub/crises-and-creativity/"
@@ -28,8 +27,9 @@ plt.axis('off')
 h = shape[0]
 w = shape[1]
 ct = 0
+
 # Function called for each successive animation frame:
-def nextFrame(arg):		   # arg is the frame number
+def nextFrame(arg): # arg is the frame number
     global h, w, image, ct
 
     plt.cla()
@@ -41,18 +41,27 @@ def nextFrame(arg):		   # arg is the frame number
     # Process every pixel
     for x in range(w):
         for y in range(h):
-            color = image.getpixel( (x,y) )
-            if color == (0, 0, 0) or color == (255, 255, 255): continue
+
+            x_r = randint(0, w-1)
+            y_r = randint(0, h-1)
+
+            #print x_r, y_r
+
+            color = image.getpixel( (x_r, y_r) )
 
             if np.random.binomial(1, 0.1) == 1:
-                if np.random.binomial(1, 0.5) == 1:
-                    image.putpixel( (x,y), (0, 0, 0))
-                elif np.random.binomial(1, 0.5) == 1:
-                    image.putpixel( (x,y), (255, 255, 255))
+                image.putpixel( (x,y), color)
+
+            #if color == (0, 0, 0) or color == (255, 255, 255): continue
+            #if np.random.binomial(1, 0.1) == 1:
+            #    if np.random.binomial(1, 0.5) == 1:
+            #        image.putpixel( (x,y), (0, 0, 0))
+            #    elif np.random.binomial(1, 0.5) == 1:
+            #        image.putpixel( (x,y), (255, 255, 255))
 
     plt.imshow(image)
 
-anim = animation.FuncAnimation(fig, nextFrame, frames=1000, interval=0, blit=False)
+anim = animation.FuncAnimation(fig, nextFrame, frames=100, interval=0, blit=False)
 #plt.show()
-anim.save(GenPath + 'image-erasure.gif', writer='imagemagick', fps=2)
+anim.save(GenPath + 'Results/gifs/image-erasure_DominanceReplacement.gif', writer='imagemagick', fps=2)
 sys.exit()
